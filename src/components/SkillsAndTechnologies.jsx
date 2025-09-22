cb3f10c5ee53f4771432b326e6953dd260b24d45
tsujin1 <justindimz01@gmail.com>
2025-09-23 01:52:45 +0800

    Update SkillsAndTechnologies.jsx

--- src/components/SkillsAndTechnologies.jsx
+++ src/components/SkillsAndTechnologies.jsx
@@ -150,6 +150,34 @@ const SkillsAndTechnologies = () => {
         color: 'text-sky-400'
       },
       {
+        name: 'Vue',
+        icon: (
+          <svg
+            className="w-8 h-8"
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            viewBox="0 0 261.76 226.69"
+            fill="currentColor"
+          >
+            <g transform="matrix(1.3333 0 0 -1.3333 -76.311 313.34)">
+              <g transform="translate(178.06 235.01)">
+                <path
+                  d="m0 0-22.669-39.264-22.669 39.264h-75.491l98.16-170.02 98.16 170.02z"
+                  fill="#41b883"
+                />
+              </g>
+              <g transform="translate(178.06 235.01)">
+                <path
+                  d="m0 0-22.669-39.264-22.669 39.264h-36.227l58.896-102.01 58.896 102.01z"
+                  fill="#34495e"
+                />
+              </g>
+            </g>
+          </svg>
+        ),
+        color: 'text-green-500'
+      },
+      {
         name: 'Tailwind CSS',
         icon: (
           <svg className="w-8 h-8" viewBox="0 0 32 32" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
